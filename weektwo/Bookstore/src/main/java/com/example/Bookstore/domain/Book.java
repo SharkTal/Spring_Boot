7229c0854acb7a5e487ee24099a05a7bd301b614
SharkTal <liutao.gao@gmail.com>
2020-09-09 15:32:18 +0300

    JPA CRUD

--- weektwo/Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
+++ weektwo/Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,11 +1,37 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private String year;
 	private String isbn;
 	private long price;
+	
+	public Book() {};
+	public Book(String title, String author, String year, String isbn, long price) {
+		super();
+		this.title= title;
+		this.author=author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getTitle() {
 		return title;
 	}
--- weektwo/Bookstore/src/main/java/com/example/Bookstore/web/BookstoreController.java
+++ weektwo/Bookstore/src/main/java/com/example/Bookstore/web/BookstoreController.java
@@ -1,34 +1,46 @@
 package com.example.Bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookstoreController {
 	
-	List<Book> books = new ArrayList<Book>();
-	@GetMapping("/index")
-	public String bookForm(Model model) {
-		Book book = new Book();
-		model.addAttribute("book", book);
-		
-		return "bookForm";
+	@Autowired
+	private BookRepository repository;
+	
+	@GetMapping(value="/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
-	@PostMapping("/index")
-	public String bookSubmit(@ModelAttribute Book book, Model model) {
-		books.add(book);
-		model.addAttribute("Books", books);
-		System.out.println(books);
-		
-		return "bookstore";
+	@GetMapping(value="/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
 	}
+	
+	@PostMapping(value="/save")
+	public String save(Book book) {
+		repository.save(book);
+		
+		return "redirect:booklist";
+	}
+	
+	@GetMapping(value="/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	
+	
 }
